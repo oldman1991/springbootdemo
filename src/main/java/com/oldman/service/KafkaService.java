d415c67c845826529fd9a8f90f9a9015a30fc9b1
lipeng <lipeng@gmei.com>
2019-07-29 20:08:41 +0800

    添加maven依赖

---
+++ src/main/java/com/oldman/service/KafkaService.java
@@ -0,0 +1,24 @@
+package com.oldman.service;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaService {
+
+    public void do_post(){
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        for (int i=0; i<100;i++){
+            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+        }
+        producer.close();
+    }
+}
